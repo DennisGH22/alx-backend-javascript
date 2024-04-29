fe7a74a730bb7c1b20137f927f94f54d5a375782
Tebogo Ndaba <dennistebogo@gmail.com>
2024-04-29 21:57:33 +0200

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,7 +4,7 @@ process.stdin.on('data', (chunk) => {
   const input = chunk.toString().trim();
 
   console.log(`Your name is: ${input}`);
-  process.exit();
+  console.log('Welcome to Holberton School, what is your name?');
 });
 
 process.stdin.on('end', () => {
