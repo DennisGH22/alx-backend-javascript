a9c3a74075f948b43341010431f6682589a84d60
Tebogo Ndaba <dennistebogo@gmail.com>
2024-04-30 00:16:47 +0200

    Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,34 +1,34 @@
 const fs = require('fs');
 
 const countStudents = (path) => {
-  try {
-    const data = fs.readFileSync(path, 'utf8');
-    const lines = data.trim().split('\n');
+  if (!fs.existsSync(path)) {
+    throw Error('Cannot load the database');
+  }
 
-    const fieldCounts = {};
+  const data = fs.readFileSync(path, 'utf8');
+  const lines = data.trim().split('\n');
 
-    lines.forEach(line => {
-      const fields = line.split(',');
-      const field = fields[fields.length - 1].trim();
+  const fieldCounts = {};
 
-      if (field !== 'undefined' && field !== 'field') {
-        if (fieldCounts[field]) {
-          fieldCounts[field].push(fields[0].trim());
-        } else {
-          fieldCounts[field] = [fields[0].trim()];
-        }
+  lines.forEach(line => {
+    const fields = line.split(',');
+    const field = fields[fields.length - 1].trim();
+
+    if (field !== 'undefined' && field !== 'field') {
+      if (fieldCounts[field]) {
+        fieldCounts[field].push(fields[0].trim());
+      } else {
+        fieldCounts[field] = [fields[0].trim()];
       }
-    });
-
-	console.log(`Number of students: ${lines.length - 1}`);
-
-    let totalStudents = 0;
-    for (const field in fieldCounts) {
-      totalStudents += fieldCounts[field].length;
-      console.log(`Number of students in ${field}: ${fieldCounts[field].length}. List: ${fieldCounts[field].join(', ')}`);
     }
-  } catch (error) {
-    throw new Error('Cannot load the database');
+  });
+
+  console.log(`Number of students: ${lines.length - 1}`);
+
+  let totalStudents = 0;
+  for (const field in fieldCounts) {
+    totalStudents += fieldCounts[field].length;
+    console.log(`Number of students in ${field}: ${fieldCounts[field].length}. List: ${fieldCounts[field].join(', ')}`);
   }
 };
 
