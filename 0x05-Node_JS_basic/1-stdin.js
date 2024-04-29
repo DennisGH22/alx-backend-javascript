79bf22568f249811c03334ae90de42e7b51bb364
Tebogo Ndaba <dennistebogo@gmail.com>
2024-04-29 23:14:13 +0200

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,7 +3,7 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('readable', () => {
   const input = process.stdin.read();
 
-  process.stdout.write(`Your name is: ${input}`);
+  input ? process.stdout.write(`Your name is: ${input}`) : null;
 });
 
 process.stdin.on('end', () => {
