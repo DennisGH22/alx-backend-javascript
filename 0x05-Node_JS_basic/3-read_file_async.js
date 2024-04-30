d9be091117b1852847917e61769d375a26cac975
Tebogo Ndaba <dennistebogo@gmail.com>
2024-05-01 01:00:04 +0200

    Reading a file asynchronously with Node JS

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,41 +1,41 @@
 const fs = require('fs');
 
 const countStudents = (path) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (err, data) => {
-      if (err) {
-        reject(new Error('Cannot load the database'));
-        return;
-      }
+	return new Promise((resolve, reject) => {
+		fs.readFile(path, 'utf8', (err, data) => {
+			if (err) {
+				reject(new Error('Cannot load the database'));
+				return;
+			}
 
-      const lines = data.trim().split('\n');
-      const students = lines.slice(1).map(line => line.split(','));
-      const numberOfStudents = students.length;
+			const lines = data.trim().split('\n');
+			const students = lines.slice(1).map(line => line.split(','));
+			const numberOfStudents = students.length;
 
-      console.log(`Number of students: ${numberOfStudents}`);
+			console.log(`Number of students: ${numberOfStudents}`);
 
-      const fieldCounts = {};
+			const fieldCounts = {};
 
-      lines.forEach(line => {
-        const fields = line.split(',');
-        const field = fields[fields.length - 1].trim();
+			lines.forEach(line => {
+				const fields = line.split(',');
+				const field = fields[fields.length - 1].trim();
 
-        if (field !== 'field') {
-          if (fieldCounts[field]) {
-            fieldCounts[field].push(fields[0].trim());
-          } else {
-            fieldCounts[field] = [fields[0].trim()];
-          }
-        }
-      });
+				if (field !== 'field') {
+					if (fieldCounts[field]) {
+						fieldCounts[field].push(fields[0].trim());
+					} else {
+						fieldCounts[field] = [fields[0].trim()];
+					}
+				}
+			});
 
-      for (const field in fieldCounts) {
-        console.log(`Number of students in ${field}: ${fieldCounts[field].length}. List: ${fieldCounts[field].join(', ')}`);
-      }
+			for (const field in fieldCounts) {
+				console.log(`Number of students in ${field}: ${fieldCounts[field].length}. List: ${fieldCounts[field].join(', ')}`);
+			}
 
-      resolve();
-    });
-  });
+			resolve();
+		});
+	});
 };
 
 module.exports = countStudents;
