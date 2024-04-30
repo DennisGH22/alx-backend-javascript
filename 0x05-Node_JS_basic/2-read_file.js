57bb67c5de294879ea4a8046ff407091a5d17ef6
Tebogo Ndaba <dennistebogo@gmail.com>
2024-05-01 00:50:58 +0200

    Reading a file asynchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -4,7 +4,11 @@ const countStudents = (path) => {
   try {
     const data = fs.readFileSync(path, 'utf8');
     const lines = data.trim().split('\n');
-    lines.shift();
+
+    const students = lines.slice(1).map(line => line.split(','));
+    const numberOfStudents = students.length;
+
+    console.log(`Number of students: ${numberOfStudents}`);
 
     const fieldCounts = {};
 
@@ -12,14 +16,14 @@ const countStudents = (path) => {
       const fields = line.split(',');
       const field = fields[fields.length - 1].trim();
 
-      if (fieldCounts[field]) {
-        fieldCounts[field].push(fields[0].trim());
-      } else {
-        fieldCounts[field] = [fields[0].trim()];
+      if (field !== 'field') {
+        if (fieldCounts[field]) {
+          fieldCounts[field].push(fields[0].trim());
+        } else {
+          fieldCounts[field] = [fields[0].trim()];
+        }
       }
     });
-
-    console.log(`Number of students: ${lines.length}`);
 
     for (const field in fieldCounts) {
       console.log(`Number of students in ${field}: ${fieldCounts[field].length}. List: ${fieldCounts[field].join(', ')}`);
