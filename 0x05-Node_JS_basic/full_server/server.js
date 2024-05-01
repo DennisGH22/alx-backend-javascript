d00e36ee8625009fbffd28ae74e9fcc4dccf7b06
Tebogo Ndaba <dennistebogo@gmail.com>
2024-05-01 03:27:58 +0200

    Organize a complex HTTP server using Express

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -6,15 +6,15 @@ const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-    req.databasePath = req.query.database || 'database.csv';
-    next();
+  req.databasePath = req.query.database || 'database.csv';
+  next();
 });
 
 app.use('/', routes);
 
 const port = 1245;
 const server = app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
 
 module.exports = server;
--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,33 +1,33 @@
 import fs from 'fs';
 
 const readDatabase = (filePath) => {
-    return new Promise((resolve, reject) => {
-        fs.readFile(filePath, 'utf8', (err, data) => {
-            if (err) {
-                reject(new Error('Cannot load the database'));
-                return;
-            }
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
 
-            const lines = data.trim().split('\n');
-            const students = {};
+      const lines = data.trim().split('\n');
+      const students = {};
 
-            lines.forEach(line => {
-                const fields = line.split(',');
-                const field = fields[fields.length - 1].trim();
+      lines.forEach(line => {
+        const fields = line.split(',');
+        const field = fields[fields.length - 1].trim();
 
-                if (field !== 'field') {
-                    const firstName = fields[0].trim();
-                    if (students[field]) {
-                        students[field].push(firstName);
-                    } else {
-                        students[field] = [firstName];
-                    }
-                }
-            });
+        if (field !== 'field') {
+          const firstName = fields[0].trim();
+          if (students[field]) {
+            students[field].push(firstName);
+          } else {
+            students[field] = [firstName];
+          }
+        }
+      });
 
-            resolve(students);
-        });
+      resolve(students);
     });
+  });
 };
 
 module.exports = readDatabase;
