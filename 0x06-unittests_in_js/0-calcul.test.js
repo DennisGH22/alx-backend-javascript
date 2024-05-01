d6649547fa6604b52ddaccebb18bddc784cc2921
Tebogo Ndaba <dennistebogo@gmail.com>
2024-05-02 00:34:26 +0200

    Initial commit

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,20 @@
+const calculateNumber = require('./0-calcul.js');
+const assert = require('assert');
+
+describe('calculateNumber', () => {
+  it('correctly adds 2 args', () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+
+  it('correctly adds 2 args', () => {
+    assert.strictEqual(calculateNumber(1, 3.7), 5)
+  });
+
+  it('correctly adds 2 args', () => {
+    assert.strictEqual(calculateNumber(1, 3.7), 5)
+  });
+
+  it('correctly adds 2 args', () => {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6)
+  });
+});
