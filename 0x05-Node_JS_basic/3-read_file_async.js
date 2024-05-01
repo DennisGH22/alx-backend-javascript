83703551cad4156aa44bb1c2a0965c15e7a6ab6a
Tebogo Ndaba <dennistebogo@gmail.com>
2024-05-01 21:50:13 +0200

    ESLint compliance

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 const countStudents = (path) => {
-  return new Promise((resolve, reject) => {
+  new Promise((resolve, reject) => {
     fs.readFile(path, 'utf8', (err, data) => {
       if (err) {
         reject(new Error('Cannot load the database'));
