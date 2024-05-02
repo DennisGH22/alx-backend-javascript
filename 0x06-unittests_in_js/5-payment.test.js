a8e4fa220858c365cc73d4d951956e5ac82c98cd
Tebogo Ndaba <dennistebogo@gmail.com>
2024-05-03 00:20:14 +0200

    Hooks

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -3,13 +3,21 @@ const { expect } = require('chai');
 const sinon = require('sinon');
 
 describe('sendPaymentRequestToApi', () => {
+  let consoleLogSpy;
+
+  beforeEach(() => {
+    consoleLogSpy = sinon.spy(console);
+  });
+
+  afterEach(() => {
+    consoleLogSpy.log.resetHistory();
+  });
+
   it('is logging the correct message "The total is: 120"', () => {
     const consoleLogSpy = sinon.spy(console);
 
     sendPaymentRequestToApi(100, 20);
     expect(consoleLogSpy.log.calledOnceWithExactly('The total is: 120'));
-
-    consoleLogSpy.log.restore();
   });
 
   it('is logging the correct message "The total is: 20"', () => {
@@ -17,7 +25,5 @@ describe('sendPaymentRequestToApi', () => {
 
     sendPaymentRequestToApi(10, 10);
     expect(consoleLogSpy.log.calledOnceWithExactly('The total is: 20'));
-
-    consoleLogSpy.log.restore();
   });
 });
